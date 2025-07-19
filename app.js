63ae774a1b0ef439a7027ef9ca2e40f332b4b51e
Luis Felipe Branquinho <branquinho1034@gmail.com>
2025-07-19 01:11:32 -0300

    Separando e criando a função Sorteador

--- app.js
+++ app.js
@@ -8,6 +8,50 @@ function sortear(){
 
     let sorteados = [];
     let numero;
+
+    if (de < ate && quantidade < (ate - de + 1)){
+        sorteador(quantidade, de, ate, sorteados, numero);
+    } else {
+        if (de > ate) {
+            alert('O campo Do Numero precisa ser menor que o campo Até o numero')
+        }
+        if ( quantidade > (de + ate)){
+            alert('Você colocar uma Quantidade maior que o Intervalo de Número')
+        }
+    }
+/*
+    if (de < ate){
+        sorteador(quantidade, de, ate, sorteados, numero);
+    } else {
+        alert('O campo Do Numero precisa ser menor que o campo Até o numero')
+    }
+*/
+}
+
+function alterarBotaoStatus(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarBotaoStatus();
+}
+
+function sorteador(quantidade, de, ate, sorteados, numero){
     for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate);
 
@@ -20,8 +64,5 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
-}
-
-function obterNumeroAleatorio(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    alterarBotaoStatus();
 }
